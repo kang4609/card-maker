ca615aab25018d27bb34a3b7a562e691ee28c4f6
kang4609 <kang4609@gmail.com>
2021-09-04 22:04:53 +0900

    Add

--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { __SECRET_INTERNALS_DO_NOT_USE_OR_YOU_WILL_BE_FIRED } from 'react-dom';
 import { useHistory } from 'react-router-dom';
 import Editor from '../editor/editor';
 import Footer from '../footer/footer';
